eaddd849f08d9a3777d7bee75749a7acb239f360
mahmoud abdelhamid <mahmoud13abdelhamid@gmail.com>
2025-01-02 06:32:02 +0200

    Add EntityFrameworkCore Packages

--- Account_Apis/obj/Debug/net8.0/Account_Apis.AssemblyInfo.cs
+++ Account_Apis/obj/Debug/net8.0/Account_Apis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Account_Apis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a38279dd47064f057ffceff91bc3c53d7ab419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19e1c998ee338e254a19d57dbc2106080b075888")]
 [assembly: System.Reflection.AssemblyProductAttribute("Account_Apis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Account_Apis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
