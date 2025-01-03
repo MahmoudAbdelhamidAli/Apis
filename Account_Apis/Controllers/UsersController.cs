8adfe88b1a27e6f5483885284219c356971ff3ab
mahmoud abdelhamid <mahmoud13abdelhamid@gmail.com>
2025-01-03 03:05:03 +0200

    added login & signup methods in Controller

--- Account_Apis/Controllers/UsersController.cs
+++ Account_Apis/Controllers/UsersController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Account_Apis.Data;
+using Account_Apis.Dtos;
+using Account_Apis.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Account_Apis.Controllers
 {
@@ -17,9 +20,86 @@ namespace Account_Apis.Controllers
             _context = context;
         }
 
-        
-        
+        // signup
+        [HttpPost]
+        [Route("signup")]
+        public async Task<IActionResult> SignUp([FromBody] UserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            // verify if user already exists or not 
+            var userExists = _context.Users.FirstOrDefault(u => u.Email == userDto.Email);
 
+            if (userExists == null)
+            {
+                var user = new User
+                {
+                    FirstName = userDto.FirstName,
+                    LastName = userDto.LastName,
+                    Email = userDto.Email,
+                    Password = userDto.Password
+                };
+
+                // addes user to the database
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest("User already exists");
+            }
+        }
+
+        // login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email && u.Password == loginDto.Password);
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest("Invalid credentials");
+            }
+        }
+        
+        // get all users
+        [HttpGet]
+        [Route("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        // get user by id
+        [HttpGet]
+        [Route("user/{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+            else
+            {
+                return Ok(user);
+            }
+        }
     }
 }
